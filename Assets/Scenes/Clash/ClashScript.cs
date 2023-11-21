3b9cf90f9489007e984f46b09dceeecb11b3a05d
MichaelKramps <mdk989@gmail.com>
2023-11-21 15:56:19 -0600

    added some more cards

--- Assets/Scenes/Clash/ClashScript.cs
+++ Assets/Scenes/Clash/ClashScript.cs
@@ -29,11 +29,11 @@ public class ClashScript : MonoBehaviour
     void Start()
     {
         // set player1 and player2
-        player1 = new ClashPlayer("10,7,7,7,10,7,10,7");
-        player2 = new ClashPlayer("8,0,4,3,7,5,2,1,6");
+        player1 = new ClashPlayer("0,8,9,11,13,12,12,8,7");
+        player2 = new ClashPlayer("6,13,7,8,5,4,1,9,2,11,12,3,0,10");
         // set animationQueue
         // clashAnimationQueue = new ClashAnimationQueue("");
-        clashAnimationQueue = new ClashAnimationQueue("p,dws,1,0*p,p,1,0*s,dws,1,0*s,p,1,0*p,dws,1,0*p,p,2,0*p,dws,1,0*p,uns,1,0*s,dws,1,0*s,pow,1,1*s,p,2,0*p,dws,1,0*p,p,3,0*p,dws,1,0*p,p,4,0*s,dws,1,0*s,pow,1,1*s,p,3,0*s,pow,2,3*p,dws,1,0*p,p,5,0*p,dws,1,0*p,p,6,0*s,dws,1,0*s,pow,1,1*s,p,4,0*s,pow,1,4*s,dws,1,0*s,pow,1,1*s,p,5,0*s,dws,1,0*s,pow,1,1*s,uns,1,0*s,dws,1,0*s,pow,1,1*s,p,6,0*b,r,1,3*b,g1,1,3*p,dws,1,0*p,p,1,0*p,dws,1,0*p,uns,1,0*b,r,2,4*b,g2,2,4*s,dws,1,0*s,pow,1,1*s,p,4,0*b,r,4,6*b,g1,4,6*p,dws,1,0*p,p,4,0*b,r,2,4*b,g1,2,4*p,dws,1,0*p,p,3,0*p,dws,1,0*p,p,2,0*b,r,4,2*b,g1,4,2*p,dws,1,0*p,p,4,0*p,dws,1,0*p,uns,1,0*b,r,4,2*b,gt,4,2*b,r,6,1*b,g2,6,1*s,dws,1,0*s,p,1,0*s,pow,1,1*b,r,1,2*b,g1,1,2*p,dws,1,0*p,p,1,0*p,1w,0,0");
+        clashAnimationQueue = new ClashAnimationQueue("p,dws,1,0*p,p,1,0*s,dws,1,0*s,p,1,0*s,pow,1,1*p,dws,1,0*p,p,2,0*s,dws,1,0*s,p,2,0*s,pow,1,2*p,dws,1,0*p,pow,1,2*p,p,3,0*s,dws,1,0*s,p,3,0*s,dws,1,0*s,uns,1,0*p,dws,1,0*p,pow,1,2*p,pow,1,1*p,p,4,0*s,dws,1,0*s,p,4,0*s,pow,3,4*p,dws,1,0*p,pow,1,2*p,pow,1,1*p,p,5,0*p,pow,1,2*p,pow,1,3*p,pow,1,4*p,pow,1,5*s,dws,1,0*s,p,5,0*s,pow,2,5*p,dws,1,0*p,pow,1,2*p,pow,1,1*p,p,6,0*s,dws,1,0*s,p,6,0*b,r,5,1*b,g1,5,1*p,dws,1,0*p,pow,1,2*p,pow,1,1*p,p,5,0*b,r,1,2*b,g1,1,2*p,dws,1,0*p,pow,1,2*p,pow,1,2*p,pow,1,2*p,pow,1,3*p,pow,1,4*p,pow,1,5*p,pow,1,6*p,p,1,0*b,r,6,4*b,g2,6,4*p,dws,1,0*p,pow,1,1*p,pow,1,2*p,pow,1,1*p,uns,1,0*p,dws,1,0*p,pow,1,1*p,pow,1,2*p,pow,1,1*p,uns,1,0*p,dws,1,0*p,pow,1,1*p,pow,1,2*p,pow,1,1*p,pow,1,1*p,pow,1,2*p,pow,1,3*p,pow,1,4*p,pow,1,5*p,pow,1,6*p,uns,1,0*s,dws,1,0*s,p,4,0*b,r,6,5*b,g1,6,5*p,dws,1,0*p,pow,1,2*p,pow,1,2*p,p,1,0*p,dws,1,0*p,pow,1,2*p,pow,1,1*p,uns,1,0*b,r,2,1*b,g1,2,1*p,dws,1,0*p,pow,1,1*p,pow,1,1*p,pow,1,3*p,pow,1,4*p,pow,1,5*p,p,2,0*b,r,2,5*b,g2,2,5*s,dws,1,0*s,pow,1,4*s,p,6,0*b,r,3,2*b,g1,3,2*p,dws,1,0*p,pow,1,1*p,pow,1,4*p,pow,1,5*p,p,3,0*b,r,5,2*b,g1,5,2*p,dws,1,0*p,pow,1,3*p,p,5,0*p,pow,1,1*p,pow,1,3*p,pow,1,4*p,pow,1,5*p,1w,0,0");
         //
 
         //create player 1 deck
